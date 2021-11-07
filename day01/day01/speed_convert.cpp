e796d758020013491618a8c1a5c9093b76ab94d6
sugu2100 <sugu2100@daum.net>
2021-11-07 15:17:34 +0900

    pdf 업로드

--- day01/day01/speed_convert.cpp
+++ day01/day01/speed_convert.cpp
@@ -1,4 +1,4 @@
-/*#include <iostream>
+#include <iostream>
 using namespace std; //소속(분류)
 #define RATE_KPH_MPH 1.609344  //매크로 상수
 
@@ -6,8 +6,6 @@ int main() {
     //공의 속도(구속) 변환 프로그램 - km -> 마일
     int kph;        //입력 변수
     double mph;    //변환 결과 - 마일의 속도
-
-    //kph = 145;
 
     cout << "당신의 구속을 입력하세요[km/h]: ";
     cin >> kph;
@@ -19,4 +17,4 @@ int main() {
     cout << "공의 속도는 " << mph << "[MPH]입니다." << endl;
 
     return 0;
-}*/
+}
